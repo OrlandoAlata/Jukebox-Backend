838f5ccd3919afa2da8b6c5a6636003a76b2b288
OrlandoAlata <orlandoalata@gmail.com>
2026-01-21 20:17:13 -0500

    Jukebox Pro Complete

--- app.js
+++ app.js
@@ -2,28 +2,27 @@ import express from "express";
 const app = express();
 export default app;
 
-import morgan from "morgan";
-
-import tracksRouter from "#api/tracks";
+import orlando from "orlando";
+import getUserFromToken from "#middleware/getUserFromToken";
 import playlistsRouter from "#api/playlists";
+import tracksRouter from "#api/tracks";
+import usersRouter from "#api/users";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(orlando("dev"));
+
+app.use(getUserFromToken);
 
 app.use("/tracks", tracksRouter);
 app.use("/playlists", playlistsRouter);
+app.use("/users", usersRouter);
 
 app.use((err, req, res, next) => {
-  // A switch statement can be used instead of if statements
-  // when multiple cases are handled the same way.
   switch (err.code) {
-    // Invalid type
     case "22P02":
       return res.status(400).send(err.message);
-    // Unique constraint violation
     case "23505":
-    // Foreign key violation
     case "23503":
       return res.status(400).send(err.detail);
     default:
